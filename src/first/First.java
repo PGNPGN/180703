2d2d8b27df58a46bc40939ab2dbcaad677beeeb9
PGNPGN <pgn0329@naver.com>
2018-07-03 15:17:41 -0500

    180703

---
+++ src/first/First.java
@@ -0,0 +1,13 @@
+package first;
+
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int a;
+			a = (int) 1.1;
+			System.out.println(a);
+			
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Gilnam
+ *
+ */
+module first {
+}
